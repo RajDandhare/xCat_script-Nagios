5b2b6bb21b64b52dfec67b170744da85b4c0a4fb
RajDandhare <145248856+RajDandhare@users.noreply.github.com>
2024-03-13 15:42:21 +0530

    Update frontend.py(V3)
    new Version of frontend.py file(V3)

--- frontend.py
+++ frontend.py
@@ -1,28 +1,59 @@
-#!/usr/bin/python3
-
 import subprocess
+def username():
+    print("---------------------- USERNAME--> root --------------------------------------")
+    pasd=input("Enter your password for compute node root user ---> ")
+    return (pasd)
 
-with open(r"ip_file", 'r') as fp:
-    ip_c = len(fp.readlines())
+def adapt():
+    adpt=int(input("please enter the host only network adapter name of master node ---> "))
+    return (adpt)
 
-with open(r"mac_file", 'r') as fp:
-    mac_c = len(fp.readlines())
+def adapt2():
+    adapt2=int(input("please enter the network adapter name of compute nodes ---> "))
+    return (adapt2)
 
-if ip_c == mac_c:
-	
-	nfs = input("Do you have external disk connected to master for NFS? [y/n]").lower()
-	if nfs == 'y' or nfs == 'yes':
-		subprocess.run(['bash','nfs.sh'])
+with open(r"mac_file",'r') as fp:
+    mac_c=len(fp.readlines())
+with open(r"ip_file",'r') as fp:
+    ip_c=len(fp.readlines())
 
-	ensp = int(input("Enter the ENS port number: "))
-	osf = int(input("Select IOS file:\n\t1.CentOS-7\n\t-> "))
-	
-	opt = int(input("Select the Booting Process\n\t1. xCAT Stateless\n\t2. xCAT Statefull\n\t-> "))
-	if opt == 1:
-		subprocess.run(['bash','xcatstateless_files/xCAT_stateless_Installation.sh',f'{ensp}',f'{mac_c}',f'{osf}',f'{nfs}'])
-	elif opt == 2:
-		subprocess.run(['bash','xcatstateless_files/xCAT_stateless_Installation.sh',f'{ensp}',f'{mac_c}',f'{osf}',f'{nfs}'])
-	else:
-		print("Please Enter the Given Options.....")
-else:
-	print("ERROR: count of ip and mac addresses are not same!!!! Check the ip_file and mac_file. ")
+def nfsfun():
+    nfs='n'
+    nfs=input("DO YOU HAVE A EXTERNAL DISK CONNECTED MASTER NODE FOR NFS --->").lower()
+    if nfs=="yes" or nfs=="y":
+            subprocess.run(['bash','nfs/nfs.sh' ])
+    return (nfs)
+
+def bootinput(password,adapter,adapter2,nfs,mac_c):
+    print("\n 1.STATE LESS\n 2.STATE FULL\n")
+    inp=int(input("ENTER THE BOOTING OPTION ---> "))
+
+    if inp==1:  # $1--adapter $2--password $3--mac_c--mac_file
+        subprocess.run([f'bash','xcatstateless_files/xCAT_stateless_Installation.sh',f'{adapter}',f'{password}',f'{mac_c}',f'{nfs}'])
+        ready_nodes="no"
+        while ready_nodes != "yes":
+            ready_nodes=input("\n   START THE NODES ""YES OR NO ---> ")
+        # $1-- mac_c --mac_file $2-- hostonly network adapter of compute nodes
+        subprocess.run([f'bash','xcatstateless_files/xcat_internet_nating.sh',f'{mac_c}',f'{adapter2}'])
+
+    elif inp==2:
+        subprocess.run([f'bash','xcatstatefull_files/xCAT_statefull_Installation.sh',f'{adapter}',f'{password}',f'{mac_c}',f'{nfs}'])
+        ready_nodes="no"
+        while ready_nodes != "yes":
+            
+            ready_nodes=input("\n   START THE NODES ""YES OR NO ---> ")
+        #script 2 # $1--mac_c--mac_file #$2-- hostonly network adpater of compute nodes
+        subprocess.run([f'bash','xcatstatefull_files/xcat_statefull_nagios_installation.sh',f'{mac_c}',f'{adapter2}'])
+        subprocess.run([f'bash','xcatstatefull_files/xcat_statefull_slurm_installation.sh',f'{mac_c}',f'{adapter2}'])
+    else:
+        print("\n PLSEASE SELECT THE OPTION........\n")
+def main():
+    if mac_c==ip_c:
+        password=username()
+        adapter=adapt()
+        adapter2=adapt2()
+        nfs=nfsfun()
+        bootinput(password,adapter,adapter2,nfs,mac_c)
+    else:
+        print("!!!! CHECK IP OR MAC FILE !!!!")   
+main()
