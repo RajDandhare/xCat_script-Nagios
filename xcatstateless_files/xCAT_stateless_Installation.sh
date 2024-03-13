bc1df80cd6810be4d46278bbb34d60decf499826
RajDandhare <145248856+RajDandhare@users.noreply.github.com>
2024-03-13 15:27:06 +0530

    Update xCAT_stateless_Installation.sh(V5)
    Updated Version of Stateless (V5)

--- xcatstateless_files/xCAT_stateless_Installation.sh
+++ xcatstateless_files/xCAT_stateless_Installation.sh
@@ -1,62 +1,90 @@
 #!/bin/bash 
+function loading_icon(){
+  count=0
+  total=34
+  #pstr="[=======================================================================]"
+   pstr="[***********************************************************************]"
+  com=$1
+  echo "$1"
+  while [ $count -lt $total ]; do
+    sleep 0.3
+    count=$(( $count + 1 ))
+    pd=$(( $count * 73 / $total ))
+    printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr
+  done
+}
+num_computes=$3
+#--------------------
 
-echo "#################################### Creating Ens.port ###########################################"
-perl -pi -e "s/BOOTPROTO=\S+/BOOTPROTO=none/ " /etc/sysconfig/network-scripts/ifcfg-ens$1
-perl -pi -e "s/ONBOOT=\S+/ONBOOT=yes/ " /etc/sysconfig/network-scripts/ifcfg-ens$1
-echo -e "IPADDR=192.168.1.1\nPREFIX=24\nIPV6_PRIVACY=no" >> /etc/sysconfig/network-scripts/ifcfg-ens$1
+# $1-- adapter --- ens port
+# $2-- password --- user password
+# $3-- mac_c -- mac_file
+# $4-- nfs 
+loading_icon " ======================= CREATING ENS. PORT =======================   "
+
+echo -e "TYPE=Ethernet\nPROXY_METHOD=none\nBROWSER_ONLY=no\nBOOTPROTO=none\nDEFROUTE=yes\nIPV4_FAILURE_FATAL=no\nIPV6INIT=yes\nIPV6_AUTOCONF=yes\nIPV6_DEFROUTE=yes\nIPV6_FAILURE_FATAL=no\nIPV6_ADDR_GEN_MODE=stable-privacy\nNAME=ens$1\nUUID=6925de50-efdc-4dd7-92eb-4683f2c649e4\nDEVICE=ens$1\nONBOOT=yes\nIPADDR=192.168.1.1\nPREFIX=24\nIPV6_PRIVACY=no" > /etc/sysconfig/network-scripts/ifcfg-ens$1
 systemctl restart network
 
-echo "#################################### Hostname ############################################"
-hostnamectl set-hostname master.demo.lab && hostname
+loading_icon " ========================= HOSTNAME ======================= "
+hostnamectl set-hostname master.demo.lab
+hostname
 
-echo "#################################### Firewalld ###########################################"
-systemctl stop firewalld && systemctl disable firewalld
+loading_icon " =================== FIREWALL ======================= "
+systemctl stop firewalld
+systemctl disable firewalld
 
-echo "#################################### Selinux #############################################"
-perl -pi -e "s/SELINUX=\S+/SELINUX=disabled/" /etc/selinux/config
+loading_icon " =================== SELINUX  ======================= "
+echo -e "SELINUX=disabled\nSELINUXTYPE=targeted" > /etc/selinux/config
 setenforce 0
+
 echo 0 > /sys/fs/selinux/enforce
 
-echo "######################################### install from http rpm file ##############################################"
+#------------------
+
+loading_icon " ======================= INSTALL FORM HTTP RMP FILE  ======================= "
 yum -y install http://build.openhpc.community/OpenHPC:/1.3/CentOS_7/x86_64/ohpc-release-1.3-1.el7.x86_64.rpm
 
-echo "#################################### installing yum-utils ##############################################"
+loading_icon " ======================= INSTALLING YUM-UTILS =======================  "
 yum -y install yum-utils
 
-echo "############################### xCat repositorys #####################################"
+loading_icon " ======================== CERTIFICATES ======================= "
 wget -P /etc/yum.repos.d https://xcat.org/files/xcat/repos/yum/latest/xcat-core/xcat-core.repo --no-check-certificate
 wget -P /etc/yum.repos.d https://xcat.org/files/xcat/repos/yum/xcat-dep/rh7/x86_64/xcat-dep.repo --no-check-certificate
 
-echo "######################### install ohpc & xCat #####################################"
-yum -y install ohpc-base xCAT && . /etc/profile.d/xcat.sh
+loading_icon  " =================INSTALL OHPC=====================   "
+yum -y install ohpc-base 
+loading_icon  " ========================== INSTALL XCAT =======================  "
+yum -y install xCAT
+loading_icon " ====================== EXECUTE XCAT SCRIPT ===================== "
+. /etc/profile.d/xcat.sh
 
-echo "################################# ntpd&chronyd service #########################"
+loading_icon " ==================== RUNNING NTPD.SERVICE ======================= "
 systemctl enable ntpd.service
 echo "server 192.168.1.1 iburst" >> /etc/ntp.conf
-systemctl restart ntpd && systemctl status ntpd
 
 echo "server 192.168.1.1 iburst" >> /etc/chrony.conf
 echo "local stratum 10" >> /etc/chrony.conf
-systemctl start chronyd.service && systemctl status chronyd.service
+systemctl restart ntpd
+systemctl start chronyd.service
 
-echo "################################# basic xcat setup #########################"
-ifconfig ens$1 192.168.1.1 netmask 255.255.255.0 up
-chdef -t site dhcpinterfaces="xcatmn|ens$1"
+loading_icon " ========================= COPY ISO IMAGE ===================  "
+copycds osimage/CentOS-7-x86_64-DVD-2009.iso
+#copycds osimage/$(ls osimage | cut -d " " -f 1 | head -$3 | tail -1 )
 
-echo "########################### copy iso image ##################################"
-copycds ios_files/$(ls ios_files | cut -d "" -f 1 | head -$3 | tail -1)
+loading_icon " ==================== EXPORTING IMAGE ==================== "
+export CHROOT=/install/netboot/centos7.9/x86_64/compute/rootimg
 
-# Creating osimages names 
-echo "osimages....."
-echo "$(lsdef -t osimage)" > osfile							    #lsdef -t osimage
+loading_icon " ===================== SET UP YOUR PASSWORD ====================== "
+chtab key=system passwd.username=root passwd.password=$2
+
+loading_icon "================= GENERTAE ISO IMAGE  ================= "
+echo "osimages....."                                             #genimage
+echo "$(lsdef -t osimage)" > osfile
 osi=$(head -2 osfile | tail -1 | cut -d " " -f 1)
-
-echo "################# Genimage ###################"
 genimage $osi
 
-echo "############################### Exporting image #################################"
-export CHROOT=/install/netboot/$(echo "$osi" | cut -d "-" -f 1)/x86_64/compute/rootimg/
-
+echo "----------------------------------------------------------------------------------------------------------"
+loading_icon "================= DOWNLOADINF BASE COMPONENTS  ================= "
 # Adding OpenHPC Componentes
 yum-config-manager --installroot=$CHROOT --enable base
 cp /etc/yum.repos.d/OpenHPC.repo $CHROOT/etc/yum.repos.d
@@ -65,22 +93,25 @@ cp /etc/yum.repos.d/epel.repo $CHROOT/etc/yum.repos.d
 # Adding OpenHPC in nodes
 cp /etc/yum.repos.d/OpenHPC.repo $CHROOT/etc/yum.repos.d
 yum -y --installroot=$CHROOT install perl
+yum -y --installroot=$CHROOT install ohpc-base-compute --skip-broken
 yum -y --installroot=$CHROOT install ntp kernel lmod-ohpc
 
 # Mounting /home and /opt/ohpc/pub to image om nodes
 echo "192.168.1.1:/home /home nfs defaults 0 0" >> $CHROOT/etc/fstab
 echo "192.168.1.1:/opt/ohpc/pub /opt/ohpc/pub nfs defaults 0 0" >> $CHROOT/etc/fstab
 
-# Exporting /home and /opt/ohpc/pub to image 
+# Exporting /home and /opt/ohpc/pub to image
 echo "/home *(rw,no_subtree_check,fsid=10,no_root_squash)" >> /etc/exports
 echo "/opt/ohpc/pub *(ro,no_subtree_check,fsid=11)" >> /etc/exports
 
-# Common HardDisk using NFS '/nfs' mounted disk for all compute nodes
+loading_icon " ========================= NFS CONFIGURE ========================= "
 if [ $4 == 'y' ] || [ $4 == 'yes' ];
 then
+  # Common HardDisk using NFS '/nfs' mounted disk for all compute nodes
   echo "192.168.1.1:/nfs /nfs nfs defaults 0 0" >> $CHROOT/etc/fstab
   echo "/nfs *(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 fi
+
 exportfs -a
 systemctl restart nfs-server && systemctl enable nfs-server && systemctl status nfs-server
 
@@ -88,7 +119,7 @@ systemctl restart nfs-server && systemctl enable nfs-server && systemctl status 
 chroot $CHROOT systemctl enable ntpd
 echo "server 192.168.1.1" >> $CHROOT/etc/ntp.conf
 
-echo "########################### Slurm_Installation_Process ##################################"
+loading_icon " ======================== Slurm_Installation_Process =========================== "
 # Munge configureation
 export MUNGEUSER=991
 groupadd -g $MUNGEUSER munge
@@ -97,7 +128,7 @@ export SLURMUSER=992
 groupadd -g $SLURMUSER slurm
 useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
 
-
+loading_icon " ============================ INSTALLING MUNGE SERVICE ============================ "
 yum install epel-release munge munge-libs munge-devel -y
 yum -y --installroot=$CHROOT install epel-release munge munge-libs munge-devel
 
@@ -105,12 +136,18 @@ yum install rng-tools -y
 rngd -r /dev/urandom
 /usr/sbin/create-munge-key -r
 dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key
-chown munge: /etc/munge/munge.key
+chown munge:munge /etc/munge/munge.key
 chmod 400 /etc/munge/munge.key
-chown -R munge: /etc/munge/ /var/log/munge/
+touch /var/log/munge/munged.log
+chown -R munge:munge /etc/munge/ /var/log/munge/
 chmod 0700 /etc/munge/ /var/log/munge/
 
 cp /etc/munge/munge.key $CHROOT/etc/munge
+cp /var/log/munge/munged.log $CHROOT/var/log/munge/
+chown -R munge:munge $CHROOT/etc/munge/ $CHROOT/var/log/munge/
+chown -R munge:munge $CHROOT/var/log/munge
+chown -R munge:munge $CHROOT/var/lib/munge
+chown -R munge:munge $CHROOT/etc/munge/
 
 systemctl enable munge && systemctl start munge
 chroot $CHROOT systemctl enable munge
@@ -118,7 +155,7 @@ chroot $CHROOT systemctl enable munge
 munge -n
 munge -n | munge
 
-echo "###################################### Slurm confirigation ############################"
+loading_icon " ======================== Slurm confirigation ======================== "
 
 yum install openssl openssl-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel man2html libibmad libibumad gcc mariadb-server mariadb-devel -y
 
@@ -128,7 +165,6 @@ yum -y install rpm-build
 rpmbuild -ta /opt/slurm-21.08.8.tar.bz2
 
 # For Master
-yum -y install rpm-build
 cd /root/rpmbuild/RPMS/x86_64/ && yum -y --nogpgcheck localinstall * && cd -
 
 # For Nodes
@@ -201,13 +237,16 @@ systemctl enable slurmctld.service
 systemctl start slurmctld.service
 systemctl status slurmctld.service
 
-echo "##################################### Nagios installtion ##########################################"
+#----------------------------------------------------------------------------------------------------------------------------------------------
+
+loading_icon " ======================= Nagios installtion ============================= "
 yum -y install ohpc-nagios
 yum -y --installroot=$CHROOT install nagios-plugins-all-ohpc nrpe-ohpc --skip-broken
 
 # Configure NRPE in compute image
 chroot $CHROOT systemctl enable nrpe
 perl -pi -e "s/^allowed_hosts=/# allowed_hosts=/" $CHROOT/etc/nagios/nrpe.cfg
+
 perl -pi -e "s/pid_file=\S+/pid_file=\/var\/run\/nrpe\/nrpe.pid/ " $CHROOT/etc/nagios/nrpe.cfg
 
 echo "nrpe 5666/tcp #NRPE" >> $CHROOT/etc/services
@@ -245,7 +284,7 @@ define hostgroup {
 # example configuration of 4 remote linux systems
 ''' > /etc/nagios/conf.d/hosts.cfg
 
-for ((i=0 ; i < $2 ; i++));
+for ((i=0 ; i < $num_computes ; i++));
 do
   n=$(($i+1))
   echo -n "c$n," >> nodenames
@@ -253,7 +292,7 @@ done
 
 perl -pi -e "s/members /members $(cat nodenames)/" /etc/nagios/conf.d/hosts.cfg
 
-for ((i=0 ; i < $2 ; i++));
+for ((i=0 ; i < $num_computes ; i++));
 do
   n=$(($i+1))
   echo -e "\ndefine host {\n use linux-box\n host_name c$n\n alias c$n\n address $(head -$n ip_file | tail -1)   ; IP address of Remote Linux host\n}"  >> /etc/nagios/conf.d/hosts.cfg
@@ -263,7 +302,7 @@ done
 perl -pi -e "s/ \/bin\/mail/ \/usr\/bin\/mailx/g" /etc/nagios/objects/commands.cfg
 
 #update email address
-perl -pi -e "s/nagios\@localhost/root\hpcsa887@gmail.com/" /etc/nagios/objects/contacts.cfg
+perl -pi -e "s/nagios\@localhost/root\@master.demo.lab/" /etc/nagios/objects/contacts.cfg
 
 # check-ssh for hosts
 echo command[check_ssh]=/usr/lib64/nagios/plugins/check_ssh localhost  >> $CHROOT/etc/nagios/nrpe.cfg
@@ -288,9 +327,9 @@ echo "/etc/passwd -> /etc/passwd" > /install/custom/netboot/compute.synclist
 echo "/etc/group -> /etc/group" >> /install/custom/netboot/compute.synclist
 echo "/etc/shadow -> /etc/shadow" >> /install/custom/netboot/compute.synclist
 
-echo "########################### Createig node #################################"
+loading_icon "================================== Createing node ====================================="
 
-for ((i=0 ; i < $2 ; i++));
+for ((i=0 ; i < $num_computes ; i++));
 do
   n=$(($i+1))
   mkdef -t node c$n groups=compute,all ip=$(head -$n ip_file | tail -1) mac=$(head -$n mac_file | tail -1) netboot=xnba arch=x86_64
@@ -302,20 +341,25 @@ chdef -t site forwarders="192.168.1.1"
 chdef -t site nameservces="192.168.1.1"
 chtab key=system passwd.username=root passwd.password=root
 
-echo "############################# packimage #############################"
+loading_icon " ======== PACK-IMAGE =============== "
 packimage $osi
 
-echo "############################ make dhcp&dns ##########################"
+loading_icon "================== RUNNING MAKE COMMAND ===============  "
 makehosts
 makenetworks
-makedhcp -a
 makedhcp -n
-makedns -a
+makedhcp -a
 makedns -n
+makedns -a
 
-echo "################# Checking is everything good[ok] #####################"
 xcatprobe xcatmn -i ens$1
 sleep 5
 
-echo "########################### Nodeset provisioning image ###############################"
+loading_icon "============ NODESET ============"
 nodeset compute osimage=$osi
+
+echo "----- FOR NAGIOS MONITORING TOOL ------"
+echo " OPEN BROWSER SEARCH "
+echo " USERNAME --- nagiosadmin "
+echo "PASSWORD --- root "
+echo "http://loaclhost/nagios"
